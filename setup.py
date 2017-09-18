f2fbc3f8187358d7320f96d191d58abb29e31a8c
Nikita Guslenko <nik.nout1@gmail.com>
2017-09-18 23:16:02 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+import frameset
+
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+def get_requirements_tests():
+    with open('requirements-tests.txt') as f:
+        return f.readlines()
+
+
+setup(
+    name='django-storages',
+    version=frameset.__version__,
+    packages=['frameset'],
+    author='Nikita Guslenko',
+    author_email='nik.nout1@gmail.com',
+    license='BSD',
+    description='Frameset Django utility',
+    long_description=read('README.rst') + '\n\n' + read('CHANGELOG.rst'),
+    url='https://github.com/guayard/frameset.git',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    tests_require=get_requirements_tests(),
+    test_suite='tests',
+    zip_safe=False
+)
